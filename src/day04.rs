cbe1c1f85f680caa9a89bcc3b1bb1538fe7b5641
Daniel Naylor <93977310+drnaylor-oc@users.noreply.github.com>
2023-12-04 13:33:32 +0000

    Day 4, Part 2

--- src/day04.rs
+++ src/day04.rs
@@ -3,7 +3,8 @@ use crate::common::load_from;
 pub fn run_day() {
     let string = load_from("day04.txt");
     let cards = parse_lines(string.as_str());
-    println!("Part 1: {}", day04a(cards));
+    println!("Part 1: {}", day04a(&cards));
+    println!("Part 2: {}", day04b(&cards));
 }
 
 fn parse_lines(string: &str) -> Vec<Card> {
@@ -29,8 +30,12 @@ struct Card {
 }
 
 impl Card {
+    fn wins(&self) -> u32 {
+        self.card_numbers.iter().filter(|number| self.winning_numbers.contains(*number)).collect::<Vec<_>>().len() as u32
+    }
+
     fn points(&self) -> u32 {
-        let wins = self.card_numbers.iter().filter(|number| self.winning_numbers.contains(*number)).collect::<Vec<_>>().len() as u32;
+        let wins = self.wins();
         if wins == 0 {
             0u32
         } else {
@@ -39,8 +44,27 @@ impl Card {
     }
 }
 
-fn day04a(input: Vec<Card>) -> u32 {
+fn day04a(input: &Vec<Card>) -> u32 {
     input.iter().map(|x| x.points()).sum()
+}
+
+fn day04b(input: &Vec<Card>) -> u32 {
+    get_card_distribution(input).iter().sum()
+}
+
+fn get_card_distribution(input: &Vec<Card>) -> Vec<u32> {
+    let mut card_numbers: Vec<u32> = vec![1u32; input.len()];
+    for (idx, card) in input.iter().enumerate() {
+        let no_of_cards = card_numbers[idx];
+        let wins = card.wins();
+        if wins > 0 {
+            for m in 1..=wins {
+                let x = card_numbers[idx + (m as usize)];
+                card_numbers[idx + (m as usize)] = x + no_of_cards;
+            }
+        }
+    }
+    card_numbers
 }
 
 #[cfg(test)]
@@ -48,7 +72,7 @@ mod tests {
     use std::collections::HashSet;
     use proptest::proptest;
     use proptest::collection::{hash_set, vec as prop_vec};
-    use crate::day04::{Card, parse_line, parse_lines};
+    use crate::day04::{Card, day04a, day04b, get_card_distribution, parse_line, parse_lines};
 
     const TEST_DATA: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n\
                              Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n\
@@ -56,6 +80,32 @@ mod tests {
                              Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n\
                              Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n\
                              Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+    #[test]
+    fn test_day_04a() {
+        let cards = parse_lines(TEST_DATA);
+        assert_eq!(day04a(&cards), 13);
+    }
+
+    #[test]
+    fn test_day_04b() {
+        let cards = parse_lines(TEST_DATA);
+        assert_eq!(day04b(&cards), 30);
+    }
+
+    #[test]
+    fn test_get_card_distribution() {
+        let cards = parse_lines(TEST_DATA);
+        let expected: Vec<u32> = vec![
+            1,
+            2,
+            4,
+            8,
+            14,
+            1
+        ];
+        assert_eq!(get_card_distribution(&cards), expected);
+    }
 
     #[test]
     fn get_cards_from_test_data() {
@@ -113,7 +163,7 @@ mod tests {
 
     proptest! {
         #[test]
-        fn test_card_power(card in 1..256u32, winning in hash_set(1..=99u32, 5), cards in hash_set(1..=99u32, 8)) {
+        fn test_card_points(card in 1..256u32, winning in hash_set(1..=99u32, 5), cards in hash_set(1..=99u32, 8)) {
             let card = Card {
                 index: card,
                 winning_numbers: winning.iter().map(|x| x.clone()).collect(),
@@ -131,6 +181,20 @@ mod tests {
                 x @ _ => panic!("There should be no more than 5 matches, but {} were found", x)
             };
             assert_eq!(card.points(), result);
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn test_card_wins(card in 1..256u32, winning in hash_set(1..=99u32, 5), cards in hash_set(1..=99u32, 8)) {
+            let card = Card {
+                index: card,
+                winning_numbers: winning.iter().map(|x| x.clone()).collect(),
+                card_numbers: cards.iter().map(|x| x.clone()).collect()
+            };
+
+            let wins: HashSet<_> = winning.intersection(&cards).collect();
+            assert_eq!(card.wins(), wins.len() as u32);
         }
     }
 
