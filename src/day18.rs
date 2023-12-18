8be37ccacf7e8f623d31e92dedd7e451fbf6c511
Daniel Naylor <93977310+drnaylor-oc@users.noreply.github.com>
2023-12-18 11:40:27 +0000

    Day 18 part b

--- src/day18.rs
+++ src/day18.rs
@@ -1,13 +1,11 @@
-use itertools::Itertools;
 use regex::Regex;
 use crate::common::load_from;
-
-type RGB = (u8, u8, u8);
 
 pub fn run_day() {
     let data = load_from("day18.txt");
     let edges = parse_instructions(data.as_str());
     println!("Part 1: {}", day_18a(&edges));
+    println!("Part 2: {}", day_18b(&edges));
 }
 
 
@@ -16,24 +14,37 @@ fn day_18a(edges: &Vec<Edge>) -> i64 {
     // then add it all together to get the total number of points, which is the area/volume.
     // use absolute due to the fact that the sign indicated direction of verticies.
     let shoelace_area: i64 = edges.iter().map(|x| x.det().clone()).sum::<i64>().abs() / 2i64;
-    let perimeter = i64::from(edges.iter().map(|x| x.first.row.abs_diff(x.last.row) + x.first.column.abs_diff(x.last.column)).map(|x| x as u32).sum::<u32>());
+    let perimeter: i64 = edges.iter().map(|x| (x.first.row - x.last.row + x.first.column - x.last.column).abs()).sum();
     let picks_internal_points: i64 = shoelace_area + 1 - (perimeter / 2);
     picks_internal_points + perimeter
 }
 
+fn day_18b(edges: &Vec<Edge>) -> i64 {
+    // shoelace to get the area (which will underestimate), pick's to get the internal points,
+    // then add it all together to get the total number of points, which is the area/volume.
+    // use absolute due to the fact that the sign indicated direction of verticies.
+    let shoelace_area: i64 = edges.iter().map(|x| x.det_hex().clone()).sum::<i64>().abs() / 2i64;
+    let perimeter: i64 = edges.iter().map(|x| (x.hex_first.row - x.hex_last.row + x.hex_first.column - x.hex_last.column).abs()).sum();
+    let picks_internal_points: i64 = shoelace_area + 1 - (perimeter / 2);
+    picks_internal_points + perimeter
+}
 
 fn parse_instructions(data: &str) -> Vec<Edge> {
     let mut row = 0i64;
     let mut column = 0i64;
 
+    let mut row_hex = 0i64;
+    let mut column_hex = 0i64;
+
     let mut edges: Vec<Edge> = Vec::new();
 
-    let pattern = Regex::new(r"^([UDLR]) (\d+) \(#([0-9a-f]{6})\)$").unwrap();
+    let pattern = Regex::new(r"^([UDLR]) (\d+) \(#([0-9a-f]{5})([0-3])\)$").unwrap();
     for line in data.lines() {
         let caps = pattern.captures(line).unwrap();
+
+        // simple
         let direction = caps.get(1).unwrap();
         let steps = str::parse::<i64>(caps.get(2).unwrap().as_str()).unwrap();
-        let colour = parse_hex(caps.get(3).unwrap().as_str());
         let last = match direction.as_str() {
             "U" => Coord { row: row - steps, column },
             "D" => Coord { row: row + steps, column },
@@ -41,37 +52,50 @@ fn parse_instructions(data: &str) -> Vec<Edge> {
             "L" => Coord { row, column: column - steps  },
             a => panic!("Unknown direction: {}", a)
         };
+
+        // hex
+        let hex_steps = i64::from_str_radix(caps.get(3).unwrap().as_str(), 16).unwrap();
+        let hex_direction = caps.get(4).unwrap();
+        let hex_last = match hex_direction.as_str() {
+            "0" => Coord { row: row_hex, column: column_hex + hex_steps },
+            "1" => Coord { row: row_hex + hex_steps, column: column_hex },
+            "2" => Coord { row: row_hex, column: column_hex - hex_steps  },
+            "3" => Coord { row: row_hex - hex_steps, column: column_hex },
+            a => panic!("Unknown direction: {}", a)
+        };
+
         let first = Coord { row, column };
+        let hex_first = Coord { row: row_hex, column: column_hex };
         row = last.row;
         column = last.column;
+        row_hex = hex_last.row;
+        column_hex = hex_last.column;
         edges.push(Edge {
             first,
             last,
-            colour
+            hex_first,
+            hex_last
         });
     }
 
     edges
 }
 
-fn parse_hex(hex: &str) -> (u8, u8, u8) {
-    (0..6)
-        .step_by(2)
-        .map(|i| u8::from_str_radix(&hex[i..i + 2], 16).unwrap())
-        .collect_tuple()
-        .unwrap()
-}
-
 #[derive(PartialEq, Debug)]
 struct Edge {
     first: Coord,
     last: Coord,
-    colour:  RGB
+    hex_first: Coord,
+    hex_last: Coord
 }
 
 impl Edge {
     fn det(&self) -> i64 {
         (self.first.row * self.last.column) - (self.first.column * self.last.row)
+    }
+
+    fn det_hex(&self) -> i64 {
+        (self.hex_first.row * self.hex_last.column) - (self.hex_first.column * self.hex_last.row)
     }
 }
 
@@ -84,12 +108,10 @@ struct Coord {
 #[cfg(test)]
 mod test {
     use std::ops::Deref;
-    use std::fmt::Write as _;
     use indoc::indoc;
-    use proptest::{prop_compose, proptest};
     use rstest::rstest;
     use structopt::lazy_static::lazy_static;
-    use crate::day18::{Coord, day_18a, Edge, parse_hex, parse_instructions};
+    use crate::day18::{Coord, day_18a, day_18b, Edge, parse_instructions};
 
     const TEST_DATA: &str = indoc! {
                             "R 6 (#70c710)
@@ -109,20 +131,20 @@ mod test {
 
     lazy_static! {
         static ref PARSED_DATA: Vec<Edge> = vec![
-            Edge { first: Coord { row: 0, column: 0 }, last: Coord { row: 0, column: 6 }, colour: (112, 199, 16) }, // R 6
-            Edge { first: Coord { row: 0, column: 6 }, last: Coord { row: 5, column: 6 }, colour: (13, 197, 113) }, // D 5
-            Edge { first: Coord { row: 5, column: 6 }, last: Coord { row: 5, column: 4 }, colour: (87, 19, 240) }, // L 2
-            Edge { first: Coord { row: 5, column: 4 }, last: Coord { row: 7, column: 4 }, colour: (210, 192, 129) }, // D 2
-            Edge { first: Coord { row: 7, column: 4 }, last: Coord { row: 7, column: 6 }, colour: (89, 198, 128) }, // R 2
-            Edge { first: Coord { row: 7, column: 6 }, last: Coord { row: 9, column: 6 }, colour: (65, 27, 145) }, // D 2
-            Edge { first: Coord { row: 9, column: 6 }, last: Coord { row: 9, column: 1 }, colour: (140, 238, 226) }, // L 5
-            Edge { first: Coord { row: 9, column: 1 }, last: Coord { row: 7, column: 1 }, colour: (202, 161, 115) }, // U 2
-            Edge { first: Coord { row: 7, column: 1 }, last: Coord { row: 7, column: 0 }, colour: (27, 88, 162) }, // L 1
-            Edge { first: Coord { row: 7, column: 0 }, last: Coord { row: 5, column: 0 }, colour: (202, 161, 113) }, // U 2
-            Edge { first: Coord { row: 5, column: 0 }, last: Coord { row: 5, column: 2 }, colour: (120, 7, 210) }, // R 2
-            Edge { first: Coord { row: 5, column: 2 }, last: Coord { row: 2, column: 2 }, colour: (167, 127, 163) }, // U 3
-            Edge { first: Coord { row: 2, column: 2 }, last: Coord { row: 2, column: 0 }, colour: (1, 82, 50) }, // L 2
-            Edge { first: Coord { row: 2, column: 0 }, last: Coord { row: 0, column: 0 }, colour: (122, 33, 227) } // U 2
+            Edge { first: Coord { row: 0, column: 0 }, last: Coord { row: 0, column: 6 }, hex_first: Coord { row: 0, column: 0}, hex_last: Coord { row: 0, column: 461937} }, // R 6 -- R 461937
+            Edge { first: Coord { row: 0, column: 6 }, last: Coord { row: 5, column: 6 }, hex_first: Coord { row: 0, column: 461937}, hex_last: Coord { row: 56407, column: 461937} }, // D 5 -- D 56407
+            Edge { first: Coord { row: 5, column: 6 }, last: Coord { row: 5, column: 4 }, hex_first: Coord { row: 56407, column: 461937}, hex_last: Coord { row: 56407, column: 818608} }, // L 2 -- R 356671
+            Edge { first: Coord { row: 5, column: 4 }, last: Coord { row: 7, column: 4 }, hex_first: Coord { row: 56407, column: 818608 }, hex_last: Coord { row: 919647, column: 818608} }, // D 2 -- D 863240
+            Edge { first: Coord { row: 7, column: 4 }, last: Coord { row: 7, column: 6 }, hex_first: Coord { row: 919647, column: 818608}, hex_last: Coord { row: 919647, column: 1186328} }, // R 2 -- R 367720
+            Edge { first: Coord { row: 7, column: 6 }, last: Coord { row: 9, column: 6 }, hex_first: Coord { row: 919647, column: 1186328}, hex_last: Coord { row: 1186328, column: 1186328} }, // D 2 -- D 266681
+            Edge { first: Coord { row: 9, column: 6 }, last: Coord { row: 9, column: 1 }, hex_first: Coord { row: 1186328, column: 1186328}, hex_last: Coord { row: 1186328, column: 609066} }, // L 5 -- L 577262
+            Edge { first: Coord { row: 9, column: 1 }, last: Coord { row: 7, column: 1 }, hex_first: Coord { row: 1186328, column: 609066}, hex_last: Coord { row: 356353, column: 609066} }, // U 2 -- U 829975
+            Edge { first: Coord { row: 7, column: 1 }, last: Coord { row: 7, column: 0 }, hex_first: Coord { row: 356353, column: 609066}, hex_last: Coord { row: 356353, column: 497056} }, // L 1 -- L 112010
+            Edge { first: Coord { row: 7, column: 0 }, last: Coord { row: 5, column: 0 }, hex_first: Coord { row: 356353, column: 497056}, hex_last: Coord { row: 1186328, column: 497056} }, // U 2 -- D 829975
+            Edge { first: Coord { row: 5, column: 0 }, last: Coord { row: 5, column: 2 }, hex_first: Coord { row: 1186328, column: 497056}, hex_last: Coord { row: 1186328, column: 5411} }, // R 2 -- L 491645
+            Edge { first: Coord { row: 5, column: 2 }, last: Coord { row: 2, column: 2 }, hex_first: Coord { row: 1186328, column: 5411}, hex_last: Coord { row: 500254, column: 5411} }, // U 3 -- U 686074
+            Edge { first: Coord { row: 2, column: 2 }, last: Coord { row: 2, column: 0 }, hex_first: Coord { row: 500254, column: 5411}, hex_last: Coord { row: 500254, column: 0} }, // L 2 -- L 5411
+            Edge { first: Coord { row: 2, column: 0 }, last: Coord { row: 0, column: 0 }, hex_first: Coord { row: 500254, column: 0}, hex_last: Coord { row: 0, column: 0} } // U 2 -- U 500254
         ];
     }
 
@@ -136,21 +158,9 @@ mod test {
         assert_eq!(day_18a(PARSED_DATA.deref()), 62);
     }
 
-    prop_compose! {
-        fn hex_strategy()(r in 0..255u8, g in 0..255u8, b in 0..255u8) -> (String, (u8, u8, u8)) {
-            let mut hex_str = String::with_capacity(6);
-            write!(&mut hex_str, "{:02x}", r).unwrap();
-            write!(&mut hex_str, "{:02x}", g).unwrap();
-            write!(&mut hex_str, "{:02x}", b).unwrap();
-            (hex_str, (r, g, b))
-        }
-    }
-
-    proptest! {
-        #[test]
-        fn test_parse_hex(hex in hex_strategy()) {
-            assert_eq!(parse_hex(hex.0.as_str()), hex.1);
-        }
+    #[test]
+    fn test_day18b() {
+        assert_eq!(day_18b(PARSED_DATA.deref()), 952408144115);
     }
 
     #[rstest]
